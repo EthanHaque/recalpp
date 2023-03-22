15b939c4cf848342111f4f6dd2a0c607c17de8f7
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-03-21 21:26:51 -0400

    Missing comma.

--- recalpp/server/recalpp_server/settings.py
+++ recalpp/server/recalpp_server/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "schedule_builder"
+    "schedule_builder",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
