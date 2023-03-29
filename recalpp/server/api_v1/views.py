563573a3b87d0018cf2af1e7d8eaefe7fc3b3ffa
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-03-29 13:39:15 -0400

    Trying to add api endpoint.

--- recalpp/server/api_v1/views.py
+++ recalpp/server/api_v1/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from . import data_producer
+
 def get_routes(request):
 
    routes=[
@@ -15,8 +17,33 @@ def get_routes(request):
          'method': 'GET',
          'body': None,
          'description': 'Returns a single course object'
+      },
+      {
+         'Endpoint': '/majors',
+         'method': 'GET',
+         'body': get_major_information,
+         'description': 'Returns dependency information about a major'
       }
       ]
 
    
    return JsonResponse(routes, safe=False)
+
+
+def get_major_information(request):
+   """Get the major information from the database.
+   
+   Parameters
+   ----------
+   request : HttpRequest
+       The request object.
+       
+   Returns
+   -------
+   major_info : dict
+       The major information including the major code, name, 
+       dependencies, etc.
+   """
+   major_code = request.GET.get('major_code')
+   major_info = data_producer.get_major_information(major_code)
+   return JsonResponse(major_info, safe=False)
