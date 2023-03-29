5f0b6722c7cee1a4a97eee58e4ce36e5a662fa1c
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-03-29 17:00:34 -0400

    Added changes for prototype.

--- recalpp/server/schedule_builder/views.py
+++ recalpp/server/schedule_builder/views.py
@@ -5,9 +5,29 @@ from django.template import loader
 import utils
 
 def index(request):
+    """The homepage for the schedule builder app.
+    
+    Parameters
+    ----------
+    request : HttpRequest
+        The request object.
+        
+    Returns
+    -------
+    HttpResponse
+        The response object.
+    """
     # connect to db
-    collection = utils.get_courses_data_collection()
+    courses_collection = utils.get_courses_data_collection()
+
+    departmental_collection = utils.get_departmental_data_collection()
+
+    major_info = departmental_collection.find_one({"type": "Major", "code": "COS-BSE"})
+
+    major_info.pop("_id")
+    # TODO: remove this from prototype
+
     # get all courses from the collection
-    courses = collection.find({})
-    context = {'name': 'Ethan Haque', 'courses': courses}
+    courses = courses_collection.find({})
+    context = {'name': 'Ethan Haque', 'courses': courses, 'degree_progress': major_info}
     return render(request, 'schedule_builder/temphomepage.html', context)
