1d444760946e3afbd0563d4b54a54eb8056ee96d
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-03-29 13:08:23 -0400

    Adding ability to get raw major information.

---
+++ recalpp/server/api_v1/data_producer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Interface to the database to retrieve data for the API."""
+
+import logging
+import os
+
+import utils
+
+
+def get_major_information(major_code: str) -> dict:
+    """Get the major information from the database.
+
+    Parameters
+    ----------
+    major_code : str
+        The major code.
+
+    Returns
+    -------
+    major_info : dict
+        The major information.
+    """
+    logging.info("Getting major information for %s.", major_code)
+    db_collection = utils.get_departmental_data_collection()
+
+    major_info = db_collection.find_one({"type": "major", "code": major_code})
+
+    if major_info:
+        logging.info("Successfully retrieved major information.")
+    else:
+        logging.error("Failed to retrieve major information.")
+
+    return major_info
