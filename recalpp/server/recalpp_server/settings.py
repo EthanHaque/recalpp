ab8f2d744dd9817ac0ae1bfb31d0ef903ce217c1
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-04-16 05:47:15 -0400

    Turning debug on.

--- recalpp/server/recalpp_server/settings.py
+++ recalpp/server/recalpp_server/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-s1k!ex=#14j4toh#gpf*wzto5$*&38!@dscl90s%aieyo+00s6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["recalpp.onrender.com", "localhost", "127.0.0.1"]
 
