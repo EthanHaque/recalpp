88e852f69c7070a5716273601b64aaef8b759e25
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-04-16 05:21:05 -0400

    Setting debug to false

--- recalpp/server/recalpp_server/settings.py
+++ recalpp/server/recalpp_server/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-s1k!ex=#14j4toh#gpf*wzto5$*&38!@dscl90s%aieyo+00s6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ["recalpp-deployment.onrender.com", "localhost", "127.0.0.1"]
 
