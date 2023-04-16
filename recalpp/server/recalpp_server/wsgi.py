9d90bf33d49fef7b2ee06c65eb0598315923a5c0
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-04-15 22:41:42 -0400

    renaming project.

--- recalpp/server/recalpp_server/wsgi.py
+++ recalpp/server/recalpp_server/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for recalpp_server project.
+WSGI config for recalpp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recalpp_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recalpp.settings")
 
 application = get_wsgi_application()
