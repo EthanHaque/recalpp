fd38a185104c5f97df2f37e33e47e99804c2cfe5
Rafay Khan <raf.d.khan10@gmail.com>
2023-04-22 17:10:26 -0400

    getCourses and internal api call

--- recalpp/server/schedule_builder/static/schedule_builder/js/course_search.js
+++ recalpp/server/schedule_builder/static/schedule_builder/js/course_search.js
@@ -24,3 +24,13 @@ function handleCourseSearch(event) {
    }
 
  }
+
+ /**
+ * Retrieves courses from the API based on the search query
+ * @param {string} search - search query
+ * @param {function} callback - function to process the data
+ */
+function getCourses(search, callback) {
+   $.getJSON("/api/v1/courses", { search: search }, callback);
+ }
+ 
