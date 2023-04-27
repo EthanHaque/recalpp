fb1ffe5754f3523470b162926f122fde51031269
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-04-27 03:19:52 -0400

    removing courses

--- recalpp/static/js/user.js
+++ recalpp/static/js/user.js
@@ -1,15 +1,16 @@
 "use strict";
 
 var User = {
-  enrolledCourses: [],
+  enrolledCourses: {},
   courseHistory: [],
   /**
-   * Returns the enrolled courses Array
-   * @returns {Array} - enrolled courses Array
+   * Returns the enrolled courses dictionary
+   * @returns {Object} - enrolled courses dictionary
    */
   getEnrolledCourses: function () {
     return User.enrolledCourses;
   },
+
   /**
    * Returns the course history array
    * @returns {Array} - course history array
@@ -17,12 +18,34 @@ var User = {
   getCourseHistory: function () {
     return User.courseHistory;
   },
+  
   /**
    * Adds a course to the enrolled courses set
+   * Adds a course to the enrolled courses dictionary
    * @param {Object} course - course object
    */
   addToEnrolledCourses: function (course) {
-    User.enrolledCourses.push(course);
+    User.enrolledCourses[course.guid] = course;
+  },
+
+  /**
+   * Removes a course from the enrolled courses dictionary
+   * @param {string} guid - course guid
+   * @returns {Object} - course object
+   */
+  removeFromEnrolledCourses: function (guid) {
+    const course = User.enrolledCourses[guid];
+    delete User.enrolledCourses[guid];
+    return course;
+  },
+
+  /**
+   * Checks if a course is in the enrolled courses dictionary
+   * @param {Object} course - course object
+   * @returns {boolean} - true if course is in the enrolled courses dictionary
+   */
+  IsEnrolledInCourse: function (course) {
+    return User.enrolledCourses.hasOwnProperty(course.guid);
   },
   /**
    * Returns the metrics object
