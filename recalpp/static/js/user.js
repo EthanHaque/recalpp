d9604052f9282f226e634229f8658c16dffdf2a9
Rafay Khan <raf.d.khan10@gmail.com>
2023-04-26 22:55:19 -0400

    tentative metric generation handled

--- recalpp/static/js/user.js
+++ recalpp/static/js/user.js
@@ -3,6 +3,7 @@
 var User = {
   enrolledCourses: [],
   courseHistory: [],
+  metrics: {},
   /**
    * Returns the enrolled courses Array
    * @returns {Array} - enrolled courses Array
@@ -24,5 +25,30 @@ var User = {
   addToEnrolledCourses: function (course) {
     User.enrolledCourses.push(course);
   },
-  
+  getMetrics: function () {
+    User.generateMetrics();
+    return User.metrics;
+  },
+  generateMetrics: function () {
+    User.parseForMetrics(User.getCourseHistory().concat(User.getEnrolledCourses()));
+  },
+  parseForMetrics: function (courseList) {
+    allMetrics = {}
+    for (const course of courseList) {
+      let courseMetrics = User.parseCourseForMetrics(course);
+      for (const metric in courseMetrics) {
+        allMetrics[metric] += courseMetrics[metric];
+      }
+    }
+    User.metrics = allMetrics;
+  },
+  parseCourseForMetrics: function (course) {
+    const metrics = {};
+
+    if (distributions.has(course.distribution_areas)) {
+      metrics[course.distribution_areas + "s"] += 1;
+    }
+
+    return metrics;
+  }
 };
