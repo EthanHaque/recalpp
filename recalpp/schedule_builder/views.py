aacbc4ec5de0159b68cf8d0e274c4a6f8850cd77
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-04-26 22:24:01 -0400

    Making login required

--- recalpp/schedule_builder/views.py
+++ recalpp/schedule_builder/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-# @login_required
+@login_required
 def index(request):
     """View function for home page of site."""
     return render(request, "schedule_builder/index.html",)
