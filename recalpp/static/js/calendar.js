689f0a8686e9620d4ef3835956b507ab588426ed
Nguyen Nguyen <nnguyenn639@gmail.com>
2023-04-28 03:36:22 -0400

    clicking one section will change color for all corresponding sections

--- recalpp/static/js/calendar.js
+++ recalpp/static/js/calendar.js
@@ -26,36 +26,33 @@ $(document).ready(function () {
       const courseIndex = calendar_event_object.event.id.split("-")[1];
       const events = User.getCourseMeetingsByGuid(guid);
       const numMeetingsForCourse = events.length;
-
+    
       const saturatedLightColor = getDesaturatedColor(events[courseIndex].color, -70);
       const saturatedDarkColor = darkenColor(saturatedLightColor);
-
-      
-
+    
       if (events[courseIndex].enrolled) {
         events[courseIndex].enrolled = false;
-        // Changes all relevant sections enrolled false
+        // Changes all relevant sections enrolled false and color to default
         for (let i = 0; i < numMeetingsForCourse; i++) {
           if (events[courseIndex].section === events[i].section) {
             events[i].enrolled = false;
+            calendar_event_object.view.calendar.getEventById(events[i].id).setProp('backgroundColor', events[i].color);
+            calendar_event_object.view.calendar.getEventById(events[i].id).setProp('borderColor', events[i].color);
+            calendar_event_object.view.calendar.getEventById(events[i].id).setProp('textColor', '#fff');
           }
         } 
       } else {
         events[courseIndex].enrolled = true;
-        // Change all relevant sections to enrolled true
+        // Change all relevant sections to enrolled true and resaturate the event
         for (let i = 0; i < numMeetingsForCourse; i++) {
           if (events[courseIndex].section === events[i].section) {
             events[i].enrolled = true;
-            // Resaturates the event
-            calendar_event_object.el.style.backgroundColor = saturatedLightColor;
-            calendar_event_object.el.style.borderColor = saturatedLightColor;
-            calendar_event_object.el.style.textColor = saturatedDarkColor;
+            calendar_event_object.view.calendar.getEventById(events[i].id).setProp('backgroundColor', saturatedLightColor);
+            calendar_event_object.view.calendar.getEventById(events[i].id).setProp('borderColor', saturatedLightColor);
+            calendar_event_object.view.calendar.getEventById(events[i].id).setProp('textColor', saturatedDarkColor);
           }
         } 
       }
-      
-      console.log(events);
-      console.log(numMeetingsForCourse);
     }
   });
   
