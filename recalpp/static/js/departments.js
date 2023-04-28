eb2b0b722e8f3ea41efeab7d703ed3c9d7f20cb3
Rafay Khan <raf.d.khan10@gmail.com>
2023-04-28 02:25:44 -0400

    tested functions and made small tweaks

--- recalpp/static/js/departments.js
+++ recalpp/static/js/departments.js
@@ -1,10 +1,10 @@
 "use strict";
 
-/* Finds the intersection of courseHistory courses and department prerequisites
+/**  Finds the intersection of courseHistory courses and department prerequisites
  * @param {Object} prereqCourses - Object of department prerequisite courses
  * @return {Object} - Object of courses in courseHistory that are also in prereqCourses
  */
-function getPrerequisitesMet(prereqCourses) {
+function getPrereqsMet(prereqCourses) {
    const courseHistory = User.getCourseHistory();
    const enrolledCourses = User.getEnrolledCourses();
    const prereqsMet = {};
@@ -15,7 +15,7 @@ function getPrerequisitesMet(prereqCourses) {
    return prereqsMet;
 }
 
-/* Adds courses to prereqsMet if they are in registeredCourses and prereqCourses
+/** Adds courses to prereqsMet if they are in registeredCourses and prereqCourses
  * @param {Object} registeredCourses - Object of registered courses
  * @param {Object} prereqCourses - Object of department prerequisite courses
  * @param {Object} prereqsMet - Object of courses in registeredCourses that are also in prereqCourses
