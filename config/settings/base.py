47724bf9c33a22763804b5c009625649e5ced173
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-05-01 21:37:46 -0400

    making ready for production.

--- config/settings/base.py
+++ config/settings/base.py
@@ -135,7 +135,11 @@ STATIC_ROOT = str(BASE_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [str(APPS_DIR / "static")]
+STATICFILES_DIRS = [
+    str(APPS_DIR / "static"),
+    # add tailwind
+    str(BASE_DIR / "theme" / "static")
+                    ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
