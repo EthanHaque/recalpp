08de395c2560ba04b3705cef287b1f4ff6265611
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-05-03 10:57:16 -0400

    Fleshing out combobox logic

--- recalpp/static/js/combobox.js
+++ recalpp/static/js/combobox.js
@@ -2,26 +2,32 @@
  * This method is used to initialize the combobox.
  */
 function init_combobox() {
-  $(".js-example-basic-multiple").select2({
-    ajax: {
-      url: `/api/v1/courses`,
-      data: function (params) {
-        if (params.term === undefined) {
-          return { term_code: currentTerm };
-        } else {
-          const parsedSearch = parseSearchString(params.term);
-          const query = constructAPIQuery(parsedSearch);
-          return { term_code: currentTerm, ...query };
-        }
+  $(".js-example-basic-multiple")
+    .select2({
+      closeOnSelect: false,
+      ajax: {
+        url: `/api/v1/courses`,
+        data: function (params) {
+          if (params.term === undefined) {
+            return { term_code: currentTerm };
+          } else {
+            const parsedSearch = parseSearchString(params.term);
+            const query = constructAPIQuery(parsedSearch);
+            return { term_code: currentTerm, ...query };
+          }
+        },
+        processResults: function (data) {
+          return {
+            results: data.map((course) => ({
+              id: JSON.stringify(course),
+              text: `${course.subject_code} ${course.catalog_number}`,
+            })),
+          };
+        },
       },
-      processResults: function (data) {
-        return {
-          results: data.map((course) => ({
-            id: course.id,
-            text: `${course.subject_code} ${course.catalog_number}`,
-          })),
-        };
-      },
-    },
-  });
+    })
+    .on("change", function (e) {
+        const selectedValues = $(this).val();
+        const courses = selectedValues.map((course) => JSON.parse(course));
+    });
 }
