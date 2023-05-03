21ea1ec78b60e0247e8f164e8399d5b27cdff3db
Nguyen Nguyen <nnguyenn639@gmail.com>
2023-05-02 22:47:26 -0400

    fixed display of distribution_areas and crosslistings_string

--- recalpp/static/js/course_search.js
+++ recalpp/static/js/course_search.js
@@ -167,17 +167,17 @@ function createCourseElement(course) {
       <div class="block w-11/12 h-max">
         <div class="pl-4 ml-px w-full border-transparent text-slate-700 group-hover:text-slate-900 dark:text-slate-400 dark:group-hover:text-slate-300 duration-75">
           <div class="flex flex-row justify-between">
-            <div class="flex-initial w-64">
-              ${course.crosslistings_string}
+            <div class="flex-initial w-48">
+              <span class="break-words">${course.crosslistings_string}</span>
             </div>
-            <div class="text-right">
-              ${course.distribution_areas}
-           </div>
-           </div>
-         </div>
-         <div class="pl-4 ml-px w-full border-transparent text-slate-700 group-hover:text-slate-900 dark:text-slate-400 dark:group-hover:text-slate-300 duration-75">
-           ${course.title}
-         </div>
+            <div class="flex-initial w-16 text-right">
+              <span class="break-all" style="white-space: nowrap">${course.distribution_areas}</span>
+            </div>
+          </div>
+        </div>
+        <div class="pl-4 ml-px w-full border-transparent text-slate-700 group-hover:text-slate-900 dark:text-slate-400 dark:group-hover:text-slate-300 duration-75">
+          ${course.title}
+        </div>
        </div>
        <button class="w-1/12 bg-indigo-500 text-white font-semibold text-xl rounded opacity-0 group-hover:opacity-100 transition-opacity duration-75 add-to-calendar" data-course='${JSON.stringify(
          {
@@ -206,7 +206,6 @@ function bindAddToCalendarEvent(selector) {
     const course = $(this).data().course;
     if (!User.isEnrolledInCourse(course)) {
       User.addToEnrolledCourses(course);
-      displayEnrolledCourses();
       addCourseToCalendar(course);
       updateEnrolledCoursesHeader();
       removeCourseFromList(course.guid);
