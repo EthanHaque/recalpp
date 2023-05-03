9db5dffd4d092260e1a675df144ddfbce29c4ffc
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-05-03 13:27:31 -0400

    Adding methods to help get and store data for user

--- recalpp/static/js/user.js
+++ recalpp/static/js/user.js
@@ -63,10 +63,18 @@ var User = {
 
   /**
    * Returns the course history dictionary
-   * @returns {Object} - course history dictionary
+   * @returns {Object} - course history dictionary where key is course guid and value is course object.
    */
   getCourseHistory: function () {
     return User.courseHistory;
+  },
+
+  /**
+   * Returns the course history count of User.
+   * @param {Object} courseHistory - course history dictionary
+   */
+  setCourseHistory: function (courseHistory) {
+    User.courseHistory = courseHistory;
   },
 
   /**
@@ -190,6 +198,51 @@ var User = {
    * @param {string} noteText - notes string
    */
   setNotes: function (notesText) {
-    User.notes = notesText; 
-  }
+    User.notes = notesText;
+  },
+
+  /**
+   * Saves the user profile to the database
+   */
+  saveUserProfile: function () {
+    $.post("/api/v1/save_user_profile/", {
+      user_data: JSON.stringify(User),
+      csrfmiddlewaretoken: User.getCookie("csrftoken"),
+    }).done(function (data) {
+      if (data.status === "success") {
+        console.log("User profile saved successfully");
+      } else {
+        console.log("Failed to save user profile");
+      }
+    });
+  },
+
+  /**
+   * Utility function to get CSRF token from cookies
+   * @param {string} name - cookie name
+   * @returns {string} - cookie value
+   */
+  getCookie: function (name) {
+    let value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  },
+
+  /**
+   * Loads the user profile from the database
+   * @returns {Object} - user profile object
+   */
+  getUserProfile: function () {
+    $.get("/api/v1/get_user_profile/")
+      .done(function (data) {
+        User.enrolledCourses = data.enrolledCourses;
+        User.courseHistory = data.courseHistory;
+        User.courseMeetings = data.courseMeetings;
+        User.notes = data.notes;
+        console.log("User profile loaded successfully");
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        console.log("Failed to load user profile:", textStatus, errorThrown);
+      });
+  },
 };
